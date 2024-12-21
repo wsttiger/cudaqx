5d287a56f02d80c48c34937b2ddaa96b5f65cdda
Scott Thornton <wsttiger@gmail.com>
2024-12-21 00:37:26 +0000

    Filled out the rest of the cuda-q kernels
    Signed-off-by: Scott Thornton <wsttiger@gmail.com>

--- libs/solvers/lib/qfd/device/qfd.cpp
+++ libs/solvers/lib/qfd/device/qfd.cpp
@@ -12,30 +12,53 @@ namespace cudaq {
 
 __qpu__ void U_m(qview<> qubits, 
 		 double dt,
-		 std::vector<double> &coefficients,
+		 const std::vector<std::complex<double>> &coefficients,
 	         const std::vector<pauli_word> &words) {
+  for (std::size_t i = 0; i < coefficients.size(); i++) {
+    exp_pauli(dt * std::real(coefficients[i]), qubits, words[i]);
+  }
 }
 
-__qpu__ void foo(qview<> qubits) {
+__qpu__ void U_n(qview<> qubits, 
+		 double dt,
+		 const std::vector<std::complex<double>> &coefficients,
+	         const std::vector<pauli_word> &words) {
+  for (std::size_t i = 0; i < coefficients.size(); i++) {
+    exp_pauli(dt * std::real(coefficients[i]), qubits, words[i]);
+  }
+}
+
+__qpu__ void apply_pauli(qview<> qubits, 
+		         const std::vector<int> &word) {
+  for (std::size_t i = 0; i < word.size(); i++) {
+    if (word[i] == 1) {
+      x(qubits[i]);
+    }
+    if (word[i] == 2) {
+      y(qubits[i]);
+    }
+    if (word[i] == 3) {
+      z(qubits[i]);
+    }
+  }
 }
 
 __qpu__ void
 qfd_kernel(double dt_alpha,
 	   double dt_beta,
-           const std::vector<double> &coefficients,
+           const std::vector<std::complex<double>> &coefficients,
 	   const std::vector<pauli_word> &words,
-	   const std::vector<pauli_word> &word_list,
+	   const std::vector<int> &word_list,
            const std::vector<double> &vec) {
   cudaq::qubit ancilla;
   cudaq::qvector qreg(vec);
   h(ancilla);
 
   x(ancilla);
-  //control(U_m, ancilla, qreg, dt_alpha, coefficients, words);
-  control(U_m, ancilla, qreg);
+  control(U_m, ancilla, qreg, dt_alpha, coefficients, words);
   x(ancilla);
 
-  // control(apply_pauli, ancilla, qreg, word_list);
-//  control(U_m, ancilla, qreg, dt_beta, coefficients, words);
+  control(apply_pauli, ancilla, qreg, word_list);
+  control(U_n, ancilla, qreg, dt_beta, coefficients, words);
 }
 } // namespace cudaq
