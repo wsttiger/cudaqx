d9afa022a46e20d292f6383173068a96a6a82ec9
Scott Thornton <wsttiger@gmail.com>
2024-12-30 19:26:33 +0000

    Almost have a compilable system - serial for now. I still have some underlying confusion about initializing state vectors
    Signed-off-by: Scott Thornton <wsttiger@gmail.com>

--- libs/solvers/lib/qfd/qfd.cpp
+++ libs/solvers/lib/qfd/qfd.cpp
@@ -7,15 +7,17 @@
  ******************************************************************************/
 
 #include "cudaq.h"
+#include "cuda-qx/core/tensor.h"
+#include "device/qfd.h"
 
 #include <iostream>
 
 namespace cudaq::solvers::qfd {	
 
-std::vector<double> term_coefficients(cudaq::spin_op op) {
-  std::vector<double> result{};
+std::vector<std::complex<double>> term_coefficients(cudaq::spin_op op) {
+  std::vector<std::complex<double>> result{};
   op.for_each_term([&](cudaq::spin_op &term) {
-    const auto coeff = term.get_coefficient().real();
+    const auto coeff = term.get_coefficient();
     result.push_back(coeff);
   });
   return result;
@@ -37,14 +39,15 @@ auto identity(std::size_t num_qubits) {
 }
 
 auto unzip_op(const cudaq::spin_op& op, std::size_t num_qubits) {
-  return std::make_tuple(term_words(op), term_coefficients(op));
+  return std::make_tuple(term_coefficients(op), term_words(op));
 }
 
 auto create_trotterized_krylov_matrix(const cudaq::spin_op& op, 
-                                      cudaq::spin_op& h_op, 
-                                      std::size_t num_qubits,
-                                      std::size_t krylov_size,
-                                      double dt) {
+                                      const cudaq::spin_op& h_op, 
+                                      const std::size_t num_qubits,
+                                      const std::size_t krylov_size,
+                                      const double dt,
+                                      const std::vector<double>& vec) {
 
   cudaq::spin_op x_0 = cudaq::spin::x(0);
   cudaq::spin_op y_0 = cudaq::spin::y(0);
@@ -54,35 +57,41 @@ auto create_trotterized_krylov_matrix(const cudaq::spin_op& op,
   auto [h_coefs, h_words]   = unzip_op(h_op, num_qubits);
 
   // Convert the op_words into int's (not sure if this is necessary)
-  std::vector<std::array<int,4>> op_words_int(op_words.size());
+  std::map<char, int> char_to_int = {{'I', 0}, {'X', 1}, {'Y', 2}, {'Z', 3}};
+  std::vector<std::vector<int>> op_words_int(op_words.size());
   std::transform(op_words.begin(), 
                  op_words.end(), 
                  op_words_int.begin(), 
-                 [] (const auto& word) {
-                   return {1,0,0,0}; 
+                 [&] (const auto& word) {
+                   std::vector<int> r(num_qubits, 0);
+                   for (std::size_t i = 0; i < num_qubits; i++) {
+                     r[i] = char_to_int[word.data()[i]];
+                   }
+                   return r;
                  });
 
-  tensor<> result({krylov_size, krylov_size});
+  cudaqx::tensor<> result({krylov_size, krylov_size});
   for (size_t m = 0; m < krylov_size; m++) {
     double dt_m = m * dt;
     for (size_t n = 0; n <= m; n++) {
       double dt_n = n * dt;
       double mat_real = 0.0;
       double mat_imag = 0.0;
-      for (std::size_t iword = 0; iword < op_words_int.size(); iword++) {
+      for (std::size_t iword = 0; iword < op_words.size(); iword++) {
+
         auto results = cudaq::observe(cudaq::qfd_kernel,
-                                      {x_0, y_0},
+                                      std::vector<cudaq::spin_op>({x_0, y_0}),
                                       dt_m,
                                       dt_n,
                                       h_coefs,
                                       h_words,
-                                      op_words_int,
+                                      op_words_int[iword],
                                       vec);
         mat_real += results[0].expectation(); 
         mat_imag += results[1].expectation();
       }
-      result({m, n}) = std::complex(mat_real, mat_imag);
-      result({n, m}) = std::conj(result({m,n});
+      // result({m, n}) = std::complex(mat_real, mat_imag);
+      // result({n, m}) = std::conj(result({m,n});
     }
   }
   return result;
