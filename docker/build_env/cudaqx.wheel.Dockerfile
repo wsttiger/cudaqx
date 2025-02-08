wsttiger/cudaqx

commit de09379bd18955e5f8b60a3cb73763d612d8089d

Update "Bump CUDA-Q commit" workflow to accept any branch name. (#75)
This will be useful for targeted testing of specific feature branches or large PRs.

---------

Signed-off-by: Ben Howe <bhowe@nvidia.com>

.github/workflows/update-cudaq-dep.yml
@@ -1,5 +1,12 @@
 on:
   workflow_dispatch:
+    inputs:
+      cudaq_branch_name:
+        type: string
+        required: true
+        description: 'Branch name for upstream CUDA-Q repo (e.g. main or pull-request/2407)'
+        default: main
+
   schedule:
     - cron: 0 1 * * 6
 
@@ -22,7 +29,7 @@ jobs:
 
       - name: Fetch the latest commit
         run: |
-          SHA=$(curl -s "https://api.github.com/repos/NVIDIA/cuda-quantum/commits/main" | jq -r '.sha')
+          SHA=$(curl -s "https://api.github.com/repos/NVIDIA/cuda-quantum/commits/${{ inputs.cudaq_branch_name }}" | jq -r '.sha')
           echo "Latest SHA: $SHA"
           echo "sha=$SHA" >> $GITHUB_ENV
 
@@ -64,8 +71,13 @@ jobs:
           GITHUB_TOKEN: ${{ github.token }}
           BRANCH_NAME: ${{ steps.commit_and_push.outputs.BRANCH_NAME }}
         run: |
+          if [[ "${{ inputs.cudaq_branch_name }}" == "main" ]]; then
+            TITLE_STR="Bump CUDA-Q commit"
+          else
+            TITLE_STR="Bump CUDA-Q commit (${{ inputs.cudaq_branch_name }} DO NOT MERGE)"
+          fi
           gh pr create \
-            --title "Bump CUDA-Q commit" \
-            --body "Auto update to the latest CUDA-Q commit" \
+            --title "${TITLE_STR}" \
+            --body "Auto update to the latest CUDA-Q commit on ${{ inputs.cudaq_branch_name }} branch" \
             --head "${BRANCH_NAME}" \
             --base "main"

commit a29cd472b0c52bba0351e3ba100869a44dd6eeb9

Include custom prebuilt CUDA-Q wheels in cudaqx-dev Python images (#81)
Also includes misc other fixes:
* Use correct Dockerfile for py-* images
* Use DOCKER_BUILDKIT=0 to workaround "too many open files" issue with
GitHub action.
* Fix bug w/ scheduled workflow introduced by #75

---------

Signed-off-by: Ben Howe <bhowe@nvidia.com>

.github/workflows/build_dev.yaml
@@ -121,13 +121,41 @@ jobs:
           echo "IMAGE_EXISTS=false" >> $GITHUB_OUTPUT
         fi
 
-    - name: Build and push
+    - name: Fetch CUDA-Q
+      uses: actions/checkout@v4
+      with:
+        repository: ${{ steps.get-cudaq-version.outputs.repo }}
+        ref: ${{ steps.get-cudaq-version.outputs.ref }}
+        path: cudaq
+        set-safe-directory: true
+
+    - name: Build CUDA-Q wheels
+      id: wheel_build
+      uses: docker/build-push-action@v5
+      with:
+        context: cudaq
+        file: cudaq/docker/release/cudaq.wheel.Dockerfile
+        build-args: |
+          base_image=ghcr.io/nvidia/cuda-quantum-devdeps:manylinux-${{ matrix.platform }}-cu12.0-gcc11-main
+          python_version=${{ matrix.python }}
+        outputs: type=local,dest=/tmp/wheels
+
+    - name: Build and push dev image with prebuilt CUDA-Q wheels
       if: ${{ inputs.force_rebuild || steps.check-image.outputs.IMAGE_EXISTS == 'false' }}
       run: |
+        # Bring wheels into upcoming Docker context
+        mkdir cudaq-wheels
+        cp /tmp/wheels/*.whl cudaq-wheels/
+
+        # Perform build
         TAGS="-t ghcr.io/nvidia/cudaqx-dev:${{ steps.get-cudaq-version-short.outputs.commit_date }}-${{ steps.get-cudaq-version-short.outputs.shortref }}-py${{ matrix.python }}-${{ matrix.platform }}"
         TAGS+=" -t ghcr.io/nvidia/cudaqx-dev:${{ steps.get-cudaq-version-short.outputs.shortref }}-py${{ matrix.python }}-${{ matrix.platform }}"
         TAGS+=" -t ghcr.io/nvidia/cudaqx-dev:latest-py${{ matrix.python }}-${{ matrix.platform }}"
-        docker build $TAGS -f docker/build_env/cudaqx.dev.Dockerfile .
+        BUILDARGS="--build-arg base_image=ghcr.io/nvidia/cuda-quantum-devdeps:manylinux-${{ matrix.platform }}-cu12.0-gcc11-main"
+        BUILDARGS+=" --build-arg python_version=${{ matrix.python }}"
+        # For some reason, this fails on amd64 unless DOCKER_BUILDKIT=0 is set.
+        # The exact error is: too many open files.
+        DOCKER_BUILDKIT=0 docker build $TAGS $BUILDARGS -f docker/build_env/cudaqx.wheel.Dockerfile .
         docker push -a ghcr.io/nvidia/cudaqx-dev
       shell: bash --noprofile --norc -euo pipefail {0}
 

.github/workflows/update-cudaq-dep.yml
@@ -29,7 +29,7 @@ jobs:
 
       - name: Fetch the latest commit
         run: |
-          SHA=$(curl -s "https://api.github.com/repos/NVIDIA/cuda-quantum/commits/${{ inputs.cudaq_branch_name }}" | jq -r '.sha')
+          SHA=$(curl -s "https://api.github.com/repos/NVIDIA/cuda-quantum/commits/${{ inputs.cudaq_branch_name || 'main' }}" | jq -r '.sha')
           echo "Latest SHA: $SHA"
           echo "sha=$SHA" >> $GITHUB_ENV
 

docker/build_env/cudaqx.wheel.Dockerfile
@@ -20,6 +20,8 @@ RUN dnf install -y jq
 RUN mkdir -p /workspaces/cudaqx
 COPY .cudaq_version /workspaces/cudaqx
 COPY .github/workflows/scripts/build_cudaq.sh /workspaces/cudaqx
+RUN mkdir /cudaq-wheels
+COPY cudaq-wheels/ /cudaq-wheels/
 
 RUN mkdir -p /workspaces/cudaqx/cudaq && cd /workspaces/cudaqx/cudaq \
   && git init \
@@ -28,5 +30,6 @@ RUN mkdir -p /workspaces/cudaqx/cudaq && cd /workspaces/cudaqx/cudaq \
   && git remote add origin https://github.com/${CUDAQ_REPO} \
   && git fetch -q --depth=1 origin ${CUDAQ_COMMIT} \
   && git reset --hard FETCH_HEAD \
-  && bash ../build_cudaq.sh --python-version ${python_version} \
-  && rm -rf build
+  && cd .. \
+  && bash build_cudaq.sh --python-version ${python_version} \
+  && rm -rf cudaq