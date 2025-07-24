97209deb6394c3a2250f2fbd54cac100ebe3185b
melody-ren <melodyr@nvidia.com>
2025-07-24 12:01:24 -0700

    Add missing licence header (#235)
    Signed-off-by: Melody Ren <melodyr@nvidia.com>

--- libs/qec/python/cudaq_qec/plugins/decoders/tensor_network_utils/tensor_network_factory.py
+++ libs/qec/python/cudaq_qec/plugins/decoders/tensor_network_utils/tensor_network_factory.py
@@ -1,3 +1,11 @@
+# ============================================================================ #
+# Copyright (c) 2025 NVIDIA Corporation & Affiliates.                          #
+# All rights reserved.                                                         #
+#                                                                              #
+# This source code and the accompanying materials are made available under     #
+# the terms of the Apache License 2.0 which accompanies this distribution.     #
+# ============================================================================ #
+
 import numpy as np
 import numpy.typing as npt
 from typing import Any, Optional
