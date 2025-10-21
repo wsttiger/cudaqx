e02b569e9f3944121dd18b766fa5fd8d2cb3e41e
Marwa Farag <46078262+marwafar@users.noreply.github.com>
2025-10-21 10:10:40 -0400

    Fixing the UHF hamiltonian generation (#309)
    In this PR:
    1- We allowed the calculation for the spin molecular Hamiltonian for
    both full and active space using UHF
    2- We fixed the electron integrals calculation for the active space
    Hamiltonian when using the UHF molecular orbitals.
    
    ---------
    
    Signed-off-by: marwafar <mfarag@nvidia.com>
    Signed-off-by: Ben Howe <bhowe@nvidia.com>
    Co-authored-by: Ben Howe <141149032+bmhowe23@users.noreply.github.com>
    Co-authored-by: Ben Howe <bhowe@nvidia.com>

--- libs/solvers/python/tests/test_molecule.py
+++ libs/solvers/python/tests/test_molecule.py
@@ -25,11 +25,11 @@ def test_operators():
                                        casci=True)
     print(molecule.hamiltonian)
     print(molecule.energies)
-    assert np.isclose(-1.11, molecule.energies['hf_energy'], atol=1e-2)
-    assert np.isclose(-1.13, molecule.energies['fci_energy'], atol=1e-2)
+    assert np.isclose(-1.1163, molecule.energies['hf_energy'], atol=1e-2)
+    assert np.isclose(-1.1371, molecule.energies['fci_energy'], atol=1e-4)
     from scipy.linalg import eigh
     minE = eigh(molecule.hamiltonian.to_matrix(), eigvals_only=True)[0]
-    assert np.isclose(-1.13, minE, atol=1e-2)
+    assert np.isclose(-1.1371, minE, atol=1e-4)
 
 
 def test_from_xyz_filename():
@@ -233,3 +233,52 @@ def test_as_with_casscf():
     print(molecule.energies)
     print(molecule.n_orbitals)
     print(molecule.n_electrons)
+
+
+def test_H2_UR():
+    geometry = [('H', (0., 0., 0.)), ('H', (0., 0., .7474))]
+    molecule = solvers.create_molecule(geometry,
+                                       'sto-3g',
+                                       0,
+                                       0,
+                                       UR=True,
+                                       ccsd=True,
+                                       casci=True,
+                                       verbose=True)
+
+    print(molecule.energies)
+    assert np.isclose(-1.1163, molecule.energies['hf_energy'], atol=1e-4)
+    assert np.isclose(-1.1371, molecule.energies['fci_energy'], atol=1e-4)
+    from scipy.linalg import eigh
+    minE = eigh(molecule.hamiltonian.to_matrix(), eigvals_only=True)[0]
+    assert np.isclose(-1.1371, minE, atol=1e-4)
+
+
+def test_N2_UR_as():
+    geometry = [('N', (0., 0., 0.56)), ('N', (0., 0., -0.56))]
+    molecule = solvers.create_molecule(geometry,
+                                       '631g',
+                                       0,
+                                       0,
+                                       UR=True,
+                                       nele_cas=4,
+                                       norb_cas=4,
+                                       ccsd=True,
+                                       casci=True,
+                                       verbose=True)
+
+    print(molecule.energies)
+    assert molecule.n_orbitals == 4
+    assert molecule.n_electrons == 4
+    assert np.isclose(molecule.energies['core_energy'], -103.31815, atol=1e-5)
+    assert np.isclose(molecule.energies['UR-CCSD'], -108.942725, atol=1e-5)
+    assert np.isclose(molecule.energies['UR-CASCI'], -108.94365, atol=1e-5)
+
+    op = solvers.jordan_wigner(molecule.hpq, molecule.hpqrs,
+                               molecule.energies['core_energy'])
+
+    assert molecule.hamiltonian == op
+
+    from scipy.linalg import eigh
+    minE = eigh(molecule.hamiltonian.to_matrix(), eigvals_only=True)[0]
+    assert np.isclose(minE, -108.9436, atol=1e-4)
