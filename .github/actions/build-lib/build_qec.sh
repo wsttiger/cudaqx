911cd91b341da2a8ee948262a75de50ac76212ee
Ben Howe <bhowe@nvidia.com>
2026-04-03 09:57:57 -0700

    Remove duplication for CUDA-Q realtime SHA
    Signed-off-by: Ben Howe <bhowe@nvidia.com>

--- .github/actions/build-lib/build_qec.sh
+++ .github/actions/build-lib/build_qec.sh
@@ -5,7 +5,7 @@ set -e
 if [ -z "$CUDAQ_REALTIME_ROOT" ]; then
   CUDAQ_REALTIME_ROOT=/tmp/cudaq-realtime
   CUDAQ_REALTIME_REPO=https://github.com/NVIDIA/cuda-quantum.git
-  CUDAQ_REALTIME_REF=82879cccdd5f51fd0f0f47d6fb756851e163edfe
+  CUDAQ_REALTIME_REF=$(jq -r '.cudaq_realtime.ref' .cudaq_version)
   _build_cwd=$(pwd)
 
   cd /tmp
